df771413060178f5b39f9e5a75f099d050204c57
Soundarya <sonushivanand2021@gmail.com>
2023-02-15 19:27:39 +0530

    This is Servlet_Calculator Project File

---
+++ src/main/java/servlet_calculator/Division.java
@@ -0,0 +1,24 @@
+package servlet_calculator;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/div")
+public class Division extends GenericServlet 
+{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		int number1=Integer.parseInt(req.getParameter("num1"));
+		int number2=Integer.parseInt(req.getParameter("num2"));
+		res.getWriter().print("<h1> The Division of"+" "+number1+" " +"and"+" "+number2+" "+"is"+" "+(number1/number2)+"</h2>");
+		
+		
+	}
+
+}
